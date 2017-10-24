71f69243c33702a4b2b0debd60b62f5d04c53cf4
Dmitry Kamaldinov <dimak24.05@mail.ru>
2017-10-24 18:28:24 +0300

    splashScreen added

---
+++ app/src/main/java/ru/mail/android/androidmailproject/SplashScreen.java
@@ -0,0 +1,10 @@
+package ru.mail.android.androidmailproject;
+
+import AppCompatActivity;
+
+/**
+ * Created by dmitrykamaldinov on 10/24/17.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+}
